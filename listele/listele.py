8d6087462c0048f2bae11c8f688d76ce08cc12c9
Esat <saygina7@gmail.com>
2026-01-31 06:45:55 +0300

    refactor: decompose 'listele' function for better readability

--- listele/listele.py
+++ listele/listele.py
@@ -58,38 +58,57 @@ def listele(li:list|dict, column:int, /, *, spaces:int=30, find:str="", reverse:
     # Print list elements top to bottom
     # It is not supported for all length and column values
     if reverse and control:
-
-        number_of_lines = math.ceil(length / column)
-        printed = 0
-        index = 0
-        while printed != length:
-            token = str(li[index])
-
-            end_of_line = (not ((printed + 1) % column))
-
-            print(token, end=("\n" if end_of_line else " " * spaces))
-            printed += 1
-
-            index += number_of_lines
-            while index >= length:
-                index -= column * number_of_lines
-                index += 1
-
-        if not end_of_line:
-            print()
+        _print_top_to_bottom(li, length, number_of_groups, column, spaces)
 
     # Print list elements left to right
     else:
         if reverse and not control:
             print("W: Reverse not supported for this input", file=sys.stderr)
 
-        for index in range(length):
-            token = str(li[index])
+        _print_left_to_right(li, length, column, spaces)
 
-            end_of_line = (not ((index + 1) % column))
 
-            print(token, end=("\n" if end_of_line else " " * spaces))
 
-        if not end_of_line:
-            print()
+def _print_top_to_bottom(
+        li: list,
+        list_length: int,
+        number_of_lines: int,
+        column: int,
+        spaces: int
+):
+    printed = 0
+    index = 0
+    while printed != list_length:
+        token = str(li[index])
+
+        end_of_line = (not ((printed + 1) % column))
+
+        print(token, end=("\n" if end_of_line else " " * spaces))
+        printed += 1
+
+        index += number_of_lines
+        while index >= list_length:
+            index -= column * number_of_lines
+            index += 1
+
+    if not end_of_line:
+        print()
+
+
+
+def _print_left_to_right(
+        li: list,
+        list_length: int,
+        column: int,
+        spaces: int
+):
+    for index in range(list_length):
+        token = str(li[index])
+
+        end_of_line = (not ((index + 1) % column))
+
+        print(token, end=("\n" if end_of_line else " " * spaces))
+
+    if not end_of_line:
+        print()
 
